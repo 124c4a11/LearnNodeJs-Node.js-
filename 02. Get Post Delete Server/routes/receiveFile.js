2eaa369efc5cfb46e9e655f1a929aa65aadb9af5
124c4a11 <124c4a11@mail.ru>
2019-03-20 15:18:07 +0300

    02. Get Post Delete Server: LimitSizeStream

--- 02. Get Post Delete Server/routes/receiveFile.js
+++ 02. Get Post Delete Server/routes/receiveFile.js
@@ -1,10 +1,41 @@
-const fs = require('fs');
+const
+  fs = require('fs'),
+  config = require('config');
+
+const LimitSizeStream = require('../LimitSizeStream');
 
 
 module.exports = function receiveFiles (filePath, req, res) {
-  const writeStream = fs.createWriteStream(filePath, { flags: 'wx' });
+  const
+    writeStream = fs.createWriteStream(filePath, { flags: 'wx' }),
+    limitStream = new LimitSizeStream({ limit: config.get('limitFileSize') });
 
-  req.pipe(writeStream);
+  req
+    .pipe(limitStream)
+    .pipe(writeStream);
+
+  limitStream.on('error', (err) => {
+    if (err.code === 'LIMIT_EXCEEDED') {
+      res.statusCode = 413;
+      res.setHeader('Connection', 'close');
+      res.end('File is too big');
+
+      fs.unlink(filepath, (err) => {});
+      return;
+    }
+
+    console.error(err);
+
+    if (!res.headersSent) {
+      res.statusCode = 500;
+      res.setHeader('Connection', 'close');
+      res.end('Internal server error');
+    } else {
+      res.end();
+    }
+
+    fs.unlink(filepath, (err) => {});
+  });
 
   writeStream.on('error', (err) => {
     if (err.code === 'EEXIST') {
